9f6b309a207d4506b410f4fa8fea59b6d16aeae3
matthew-shaw <matthew.shaw@digital.landregistry.gov.uk>
2015-01-30 10:16:32 +0000

    Install virtualenv, pip, flask, pymongo

--- Vagrantfile
+++ Vagrantfile
@@ -4,8 +4,12 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.box = "ubuntu/trusty64"
   config.vm.provision :shell, :path => 'provision.sh'
 
+  #MongoDB Ports
   config.vm.network :forwarded_port, host: 27017, guest: 27017
   config.vm.network :forwarded_port, host: 27018, guest: 27018
   config.vm.network :forwarded_port, host: 27019, guest: 27019
   config.vm.network :forwarded_port, host: 28017, guest: 28017
+
+  # Flask Port
+  config.vm.network :forwarded_port, host: 5000, guest: 5000
 end
--- provision.sh
+++ provision.sh
@@ -5,3 +5,15 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
+
+echo "Install Python PIP"
+apt-get -y install python-pip python-dev build-essential
+
+echo "Install Virtualenv"
+pip install --upgrade virtualenv
+
+echo "Install Flask"
+pip install --upgrade flask
+
+echo "Install Pymongo"
+pip install --upgrade pymongo
